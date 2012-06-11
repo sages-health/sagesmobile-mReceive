adeaef825451cc9324b8becfdd83890a259382e6
POKUAM1 <POKUAM1@POKUAM1-WL1.dom1.jhuapl.edu>
2012-06-11 19:08:45 -0400

    supporting unicode character blocks

--- rapidjava/org.rapidsms.java/src/org/rapidsms/java/core/parser/interpreter/AlphaNumericInterpreter.java
+++ rapidjava/org.rapidsms.java/src/org/rapidsms/java/core/parser/interpreter/AlphaNumericInterpreter.java
@@ -3,6 +3,9 @@ package org.rapidsms.java.core.parser.interpreter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.lang.StringUtils;
+
+
 /**
  * @author Adjoa Poku adjoa.poku@jhuapl.edu
  * @created June 2011 Summary:
@@ -18,9 +21,43 @@ public class AlphaNumericInterpreter implements IParseInterpreter {
 
 	Pattern mPattern;
 
+	private static String[] unicodeBlocks = {"Basic Latin",
+		"Latin-1 Supplement",
+		"Latin Extended-A",
+		"Latin Extended-B",
+//		"IPA Extensions",
+//		"Spacing Modifier Letters",
+//		"Combining Diacritical Marks",
+//		"Greek",
+//		"Cyrillic",
+//		"Khmer",
+//		"Latin Extended Additional",
+//		"Greek Extended",
+//		"General Punctuation",
+//		"Superscripts and Subscripts",
+//		"Currency Symbols",
+//		"Combining Marks for Symbols",
+//		"Letterlike Symbols",
+//		"Number Forms",
+//		"Arrows",
+//		"Mathematical Operators",
+//		"Miscellaneous Technical",
+//		"Optical Character Recognition",
+//		"Enclosed Alphanumerics",
+		"Hangul Jamo",
+		"Hangul Compatibility Jamo",
+		"Box Drawing",
+		"Block Elements",
+		"Geometric Shapes",
+		"Miscellaneous Symbols",
+		"Dingbats"	};
+	
 	public AlphaNumericInterpreter() {
+		String unicodeBlockPullin = "\\p{In" + StringUtils.join(unicodeBlocks, "}\\p{In") + "}";
+		System.out.println("ALPHANUMERIC INTERPRETER: FOOBAR>> " + unicodeBlockPullin);
 		// MUST HAVE ZERO GROUPS AND OTHER TOKENIZING JUNK
-		mPattern = Pattern.compile("[A-Za-z\\d]+\\.*");
+		mPattern = Pattern.compile("([A-Za-z\\d" + unicodeBlockPullin + "]+)\\.*");
+//		mPattern = Pattern.compile("[A-Za-z\\d]+\\.*");
 	}
 
 	public Object interpretValue(String token) {
